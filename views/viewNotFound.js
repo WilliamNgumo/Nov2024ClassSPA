ff2b3441a8118f428665b5b0e0c8001cb720c236
William Ngumo <williamngumo@Mac.lan>
2025-01-15 18:25:05 -0600

    Latest Changes

--- views/viewNotFound.js
+++ views/viewNotFound.js
@@ -1,8 +1,9 @@
 import html from "html-literal";
+import notFoundRobot from "../assets/img/oops-404.jpg";
 
 export default () => html`<body>
 <div id="oops404">
-  <img src="assets/img/oops-404.jpg" alt="View not found!">
+  <img src="${notFoundRobot}" alt="View not found!">
   <div class="attribution">
     <a
       href="https://www.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_13315300.htm"
